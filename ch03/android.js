045453ef6433e44f3ee02f53dc8714e2feeb6748
Jonathan Stark <jstark@jonathanstark.com>
2012-01-02 13:38:00 -0500

    example 3-9

--- ch03/android.js
+++ ch03/android.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 });
 function loadPage(url) {
     $('body').append('<div id="progress">Loading...</div>');
+    scrollTo(0,0);
     if (url == undefined) {
         $('#container').load('index.html #header ul', hijackLinks);
     } else {
@@ -11,8 +12,11 @@ function loadPage(url) {
 }
 function hijackLinks() {
     $('#container a').click(function(e){ 
-        e.preventDefault(); 
-        loadPage(e.target.href);
+        var url = e.target.href;
+        if (url.match(window.location.hostname)) {
+            e.preventDefault();
+            loadPage(url);
+        }
     });
     var title = $('h2').html() || 'Hello!';
     $('h1').html(title);
