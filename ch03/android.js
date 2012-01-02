e7815182c4924f6c99846c068882ba8486276d56
Jonathan Stark <jstark@jonathanstark.com>
2012-01-02 13:44:27 -0500

    example 3-13

--- ch03/android.js
+++ ch03/android.js
@@ -1,25 +1,38 @@
-$(document).ready(function(){ 
-    loadPage();
+var hist = []; 
+var startUrl = 'index.html'; 
+$(document).ready(function(){
+    loadPage(startUrl);
 });
 function loadPage(url) {
-    $('body').append('<div id="progress">Loading...</div>');
-    scrollTo(0,0);
-    if (url == undefined) {
-        $('#container').load('index.html #header ul', hijackLinks);
+    $('body').append('<div id="progress">Loading...</div>'); 
+    scrollTo(0,0); 
+    if (url == startUrl) {
+        var element = ' #header ul'; 
     } else {
-        $('#container').load(url + ' #content', hijackLinks);
-    } 
-}
-function hijackLinks() {
-    $('#container a').click(function(e){ 
-        var url = e.target.href;
-        if (url.match(window.location.hostname)) {
-            e.preventDefault();
-            loadPage(url);
-        }
+        var element = ' #content';
+    }
+    $('#container').load(url + element, function(){
+        var title = $('h2').html() || 'Hello!'; 
+        $('h1').html(title); 
+        $('h2').remove(); 
+        $('.leftButton').remove(); 
+        hist.unshift({'url':url, 'title':title}); 
+        if (hist.length > 1) {
+            $('#header').append('<div class="leftButton">'+hist[1].title+'</div>'); 
+            $('#header .leftButton').click(function(e){
+                $(e.target).addClass('clicked');
+                var thisPage = hist.shift(); 
+                var previousPage = hist.shift(); 
+                loadPage(previousPage.url);
+            });
+        } 
+        $('#container a').click(function(e){
+            var url = e.target.href; 
+            if (url.match(window.location.hostname)) {
+                e.preventDefault(); 
+                loadPage(url);
+            }
+        });
+        $('#progress').remove();
     });
-    var title = $('h2').html() || 'Hello!';
-    $('h1').html(title);
-    $('h2').remove();
-    $('#progress').remove();
 }
